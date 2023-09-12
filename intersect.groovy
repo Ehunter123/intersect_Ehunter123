3a4de72e8e05b2d79e5118d506c860cad21193b0
Maura Hunter <Maura Hunter@10.0.0.146>
2023-09-12 18:00:27 -0400

    I made the square bigger and moves the spheres a bit

--- intersect.groovy
+++ intersect.groovy
@@ -1,10 +1,10 @@
-double size =40;
+double size =90;
 CSG cube = new Cube(	size,// X dimention
 			size,// Y dimention
 			size//  Z dimention
 			).toCSG()
 //create a sphere
-CSG sphere = new Sphere(size/20*12.5).toCSG()
+CSG sphere = new Sphere(size/60*12.5).toCSG()
 // perform a union
 CSG cubePlusSphere = cube.intersect(sphere);
 
